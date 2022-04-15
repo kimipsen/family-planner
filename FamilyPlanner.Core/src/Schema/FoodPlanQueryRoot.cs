b276af513f89a587a8d030252518488f8619d1f7
Kim Ipsen <kim.ipsen@outlook.dk>
2022-04-15 19:18:47 +0200

    Workflows for sonarcloud

--- FamilyPlanner.Core/src/Schema/FoodPlanQueryRoot.cs
+++ FamilyPlanner.Core/src/Schema/FoodPlanQueryRoot.cs
@@ -6,5 +6,6 @@ namespace FamilyPlanner.Core.Schema;
 [ExtendObjectType(typeof(FamilyPlannerQueryRoot))]
 public class FoodPlanQueryRoot
 {
+	public FoodPlan? GetFoodPlan(FamilyPlannerContext context, int id) => context.FoodPlans.SingleOrDefault(x => x.Id == id);
 	public FoodPlan? GetActiveFoodPlan(FamilyPlannerContext context) => context.FoodPlans.SingleOrDefault(x => x.Active);
 }
